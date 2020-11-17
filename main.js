1d55835169ea4d1e4095bb5028aede16935b80a2
Trehan <Anirudh.Trehan@philips.com>
2020-11-17 08:30:53 +0530

    jenkins build

---
+++ main.js
@@ -0,0 +1,43 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var electron_1 = require("electron");
+var path = __importStar(require("path"));
+var mainWindow;
+function createWindow() {
+    mainWindow = new electron_1.BrowserWindow({
+        height: 600,
+        width: 800,
+    });
+    mainWindow.loadFile(path.join(__dirname, "/index.html"));
+    mainWindow.webContents.openDevTools();
+}
+electron_1.app.on("ready", createWindow);
+electron_1.app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") {
+        electron_1.app.quit();
+    }
+});
+electron_1.app.on("activate", function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
